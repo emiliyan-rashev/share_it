fe69ab5b73e7813b075277b6b217683b488c8482
Emiliyan Rashev <e.a.rashev@abv.bg>
2023-08-28 02:15:45 +0300

    Add default page

--- share_it/urls.py
+++ share_it/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-
+from django.shortcuts import redirect
+from django.urls import path, include, reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url=reverse_lazy("expenses:list")), name="home"),
     path("expenses/", include("expenses.urls", namespace="expenses")),
     path("utils/", include("utils.urls", namespace="utils")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
