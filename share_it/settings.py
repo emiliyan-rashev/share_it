dfcaa459cf0e075345eac7f7a0d15b5588d349ae
Emiliyan Rashev <e.a.rashev@abv.bg>
2025-01-26 16:19:26 +0200

    Add csrf trusted origins

--- share_it/settings.py
+++ share_it/settings.py
@@ -122,3 +122,4 @@ SECURE_HSTS_SECONDS = 10
 SECURE_SSL_REDIRECT = False
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 LOGIN_URL = reverse_lazy("admin:login")
+CSRF_TRUSTED_ORIGINS = ["https://emiliyan-rashev.com/"]
