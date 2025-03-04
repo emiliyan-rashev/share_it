6d5a83c54718bb3a38d76d5a16f5b295eefeb269
Emiliyan Rashev <e.a.rashev@abv.bg>
2025-03-04 19:19:23 +0200

    Add payments url

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,7 @@ from users.models import User
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    fields = ("email", "first_name", "last_name", "avatar")
+    fields = ("email", "first_name", "last_name", "payment_url", "avatar")
 
 
 admin.site.register(User, CustomUserAdmin)
